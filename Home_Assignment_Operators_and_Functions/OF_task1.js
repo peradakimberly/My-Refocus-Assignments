4e329a855154fe499bbe9fbf356c60f4c398462a
peradakimberly <peradakimberly@gmail.com>
2022-10-18 05:44:45 +0800

    My First Assignments

---
+++ Home_Assignment_Operators_and_Functions/OF_task1.js
@@ -0,0 +1,17 @@
+function convertToKelvinC(tempCelsius)
+{
+    return tempCelsius+273.15;
+}
+
+console.log(convertToKelvinC(600));
+console.log(convertToKelvinC(743));
+
+
+function convertToKelvinF(tempFarenheit)
+{
+    let Farenheit = tempFarenheit+459.67;
+    return Kelvin = Farenheit*0.5556
+}
+
+console.log(convertToKelvinF(193));
+console.log(convertToKelvinF(840));
---
+++ Home_Assignment_Operators_and_Functions/OF_task2.js
@@ -0,0 +1,7 @@
+function computeTip(totalBill)
+{   
+    return totalBill*0.10;
+}
+
+console.log(computeTip(700));
+console.log(computeTip(2573));
